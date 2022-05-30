ed633a4c21a4a338a41152d8d7f9bd4f578b3da8
mohammed-taherali <mohammedtaherali@gmail.com>
2022-05-30 08:37:15 +0530

    added features

--- app.py
+++ app.py
@@ -9,8 +9,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    print("first branch")
     return render_template("index.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+# hello world
