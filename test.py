e16c697dc15113455857651a0e1e26f007dbc33e
mohammed-taherali <mohammedtaherali@gmail.com>
2022-06-09 10:00:06 +0530

    testing change

--- test.py
+++ test.py
@@ -7,6 +7,22 @@
 
 # print(lst)
 
-t = "fas"
+# t = "fas"
 
-print(float(t))
+# print(float(t))
+
+# abcd = "None"
+
+# if abcd:
+#     print("hi")
+
+# else:
+#     print("BYE!")
+
+from werkzeug.security import generate_password_hash,check_password_hash
+
+hash_pass = generate_password_hash("Mohammed")
+print(hash_pass)
+# print(check_password_hash(hash_pass, "mohammed"))
+
+print(check_password_hash("pbkdf2:sha256:260000$wSkbU8M4q5mwpBTr$ace9fa7841a7bb058f1df4126157e6f1a5c0782b9080cc0fb5bb3a49571e0763", "jhalrapatan420"))
